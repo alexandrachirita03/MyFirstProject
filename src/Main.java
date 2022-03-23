123a2239d4b4befe6b1f8a18cf3a3077becbef4a
alexandrachirita03 <alexandra_rusitoru@yahoo.com>
2022-03-23 23:13:46 +0200

    My first commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+        System.out.println("Alexandra");
+
+// Sum of the two numbers
+        int a = 27;
+        int b = 19;
+        int sum = a + b;
+        System.out.println(sum);
+
+        // Divide two numbers
+
+        float x = 57;
+        float y = 12;
+        float d = x / y;
+        System.out.println(d);
+
+        // Print the result
+
+        int result = -5 + 8 * 6;
+        System.out.println("result=" + (-5 + 8 * 6));
+
+        int result1 = (55 + 9) % 9;
+        System.out.println("result1=" + (55 + 9) % 9);
+
+        int result2 = 20 + -3 * 5 / 8;
+        System.out.println("result2=" + (20 + -3 * 5 / 8));
+
+        int result3 = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println("result3=" + (5 + 15 / 3 * 2 - 8 % 3));
+    }
+}
